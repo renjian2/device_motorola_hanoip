d8a005e7979faf93c8f3aa4d7acc116ff08d96e2
Nikhil <renjian503@gmail.com>
2022-05-02 10:28:15 +0530

    disable_configstore

--- device.mk
+++ device.mk
@@ -270,6 +270,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0.vendor \
     vendor.qti.hardware.display.mapperextensions@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
