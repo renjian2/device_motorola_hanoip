ef391a2bc861e320f3494c9171197e4de132b3f1
Nikhil <renjian503@gmail.com>
2022-05-09 12:19:30 +0530

    update

--- device.mk
+++ device.mk
@@ -312,21 +312,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
     android.hardware.gnss@2.1-service-qti \
-    gnss@2.0-base.policy \
-    gnss@2.0-xtra-daemon.policy \
-    gnss@2.0-xtwifi-client.policy \
-    gnss@2.0-xtwifi-inet-agent.policy \
     libbatching \
     libgeofencing \
     libgnss \
     libgnsspps \
-    liblocdiagiface \
     libsynergy_loc_api
 
 PRODUCT_PACKAGES += \
     apdr.conf \
     flp.conf \
-    gnss_antenna_info.conf \
     gps.conf \
     izat.conf \
     lowi.conf \
